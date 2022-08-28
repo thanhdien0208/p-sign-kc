4d011dc17cc9b44cc7f516b984bed8c7ad98d4b4
thanhdien0208 <diennguyen.blog@gmail.com>
2022-08-28 22:18:20 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,25 @@ function myFunction() {
     }
   }
 
+//btnDropdown
+function btnDropdown() {
+  document.getElementById("btnUpDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 // Navlink
 var header = document.getElementById("navLink");
 var btns = header.getElementsByClassName("nav_item");
