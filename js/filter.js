ba3c5258f95eafa4c4b2e2cc443640713fbfc18a
thanhdien0208 <diennguyen.blog@gmail.com>
2022-09-05 11:38:22 +0700

    upload

---
+++ js/filter.js
@@ -0,0 +1,15 @@
+$(document).ready(function($) {
+    // $('table').hide();
+    
+    $('#statusSelector').change( function(){
+      var selection = $(this).val();
+      $('table')[selection? 'show' : 'hide']();
+      
+      if (selection) {
+        $.each($('#statusTable tbody tr'), function(index, item) {
+          $(item)[$(item).is(':contains('+ selection  +')')? 'show' : 'hide']();
+        });
+      }
+        
+    });
+});
